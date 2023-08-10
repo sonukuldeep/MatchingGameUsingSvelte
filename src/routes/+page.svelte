aff7ac6d5581158e17c7ce2e9033739c09fb2d58
sonukuldeep <sonukuldeep@outlook.com>
2023-08-10 21:32:32 +0530

    game complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,20 @@
     type State = "start" | "playing" | "paused" | "won" | "lost";
 
     let state: State = "start";
-    let size = 20;
+    let size = 10;
     let grid = createGrid();
     let maxMatches = grid.length / 2;
     let selected: number[] = [];
     let matches: string[] = [];
+    let timerId: number | null = null;
+    let time = 20;
+
+    function startGameTimer() {
+        function coundDown() {
+            state !== "paused" && (time -= 1);
+        }
+        timerId = setInterval(coundDown, 1000);
+    }
 
     function createGrid() {
         let cards = new Set<string>();
@@ -39,15 +48,54 @@
         }, 300);
     }
 
+    function resetGame() {
+        timerId && clearInterval(timerId);
+        grid = createGrid();
+        maxMatches = grid.length / 2;
+        selected = [];
+        matches = [];
+        timerId = null;
+        time = 20;
+    }
+
     function gameWon() {
         state = "won";
+        resetGame();
+    }
+
+    function gameLost() {
+        state = "lost";
+        resetGame();
+    }
+
+    function pauseGame(e: KeyboardEvent) {
+        if (e.key === "Escape") {
+            switch (state) {
+                case "playing":
+                    state = "paused";
+                    break;
+                case "paused":
+                    state = "playing";
+                    break;
+            }
+        }
+    }
+
+    $: if (state === "playing") {
+        !timerId && startGameTimer();
     }
 
     $: selected.length === 2 && matchCards();
     $: maxMatches === matches.length && gameWon();
-
-    $: console.log(state, selected, matches);
+    $: time === 0 && gameLost();
+    
 </script>
+
+<svelte:window on:keydown={pauseGame} />
+
+{#if state === "paused"}
+    <h1>Game paused</h1>
+{/if}
 
 {#if state === "start"}
     <h1>Matching game</h1>
@@ -59,6 +107,9 @@
 {/if}
 
 {#if state === "playing"}
+    <h1 class="timer" class:pulse={time <= 10}>
+        {time}
+    </h1>
     <div class="matches">
         {#each matches as card}
             <div>{card}</div>
@@ -74,9 +125,10 @@
                 class="card"
                 class:selected={isSelected}
                 disabled={isSelectedOrMatched}
+                class:flip={isSelectedOrMatched}
                 on:click={() => selectCard(cardIndex)}
             >
-                <div class:match>{card}</div>
+                <div class:match class="back">{card}</div>
             </button>
         {/each}
     </div>
@@ -103,7 +155,11 @@
         width: 140px;
         font-size: 4rem;
         background-color: var(--bg-2);
+
+        transition: rotate 0.3s ease-out;
+        transform-style: preserve-3d;
     }
+
     .card.selected {
         border: 4px solid var(--border);
     }
@@ -112,10 +168,39 @@
         opacity: 0.4;
     }
 
+    .card.flip {
+        rotate: y 180deg;
+        pointer-events: none;
+    }
+
+    .card .back {
+        position: absolute;
+        inset: 0;
+        display: grid;
+        place-content: center;
+        backface-visibility: hidden;
+        rotate: y 180deg;
+    }
+
     .matches {
         display: flex;
         gap: 1rem;
         margin-block: 2rem;
         font-size: 3rem;
+    }
+
+    .timer {
+        transition: color 0.3s ease;
+    }
+
+    .pulse {
+        color: var(--pulse);
+        animation: pulse 1s infinite ease;
+    }
+
+    @keyframes pulse {
+        to {
+            scale: 1.4;
+        }
     }
 </style>
